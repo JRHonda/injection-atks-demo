815965309b8d0189d599351f6d4c3c0b2377d8e6
Justin Honda <justinohonda@gmail.com>
2019-09-29 00:07:35 -0400

    Updates

---
+++ precache-manifest.970ea006a03cec523ccf2300338f86c5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c862837fd937f9451fddac145381399",
+    "url": "/injection-atks-demo/index.html"
+  },
+  {
+    "revision": "546a5a0eb42a9ed51124",
+    "url": "/injection-atks-demo/static/js/2.adf57dcb.chunk.js"
+  },
+  {
+    "revision": "8f482fd29d695bcb31c8",
+    "url": "/injection-atks-demo/static/js/main.7cb3825c.chunk.js"
+  },
+  {
+    "revision": "a12ba34f2744271706ed",
+    "url": "/injection-atks-demo/static/js/runtime-main.b7f2f0f8.js"
+  }
+]);
